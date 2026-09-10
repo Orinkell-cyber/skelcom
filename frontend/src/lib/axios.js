518a0e3c1376cddbdd9e0e70f85f5c2f667fa325
Orinkell-cyber <orinkellsafou5@gmail.com>
2026-09-10 14:35:25 +0000

    new backend commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
   withCredentials: true,
 });
 
