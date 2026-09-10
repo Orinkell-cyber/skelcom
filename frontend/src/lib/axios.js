a44145de067997e9d3796796275e66c6c5abac81
Orinkell-cyber <orinkellsafou5@gmail.com>
2026-09-10 14:06:18 +0000

    Update axios baseURL to production URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: https://skelcom.onrender.com || "http://localhost:5000/api",
   withCredentials: true,
 });
 
